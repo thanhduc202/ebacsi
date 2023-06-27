b0d5feff89dad59d46fc8ab48019ceebb9708e9d
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 23:35:11 +0700

    format code

--- src/main/java/com/thanh/ebacsi/repository/PostRepository.java
+++ src/main/java/com/thanh/ebacsi/repository/PostRepository.java
@@ -15,17 +15,25 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p from Post p ")
     List<PostResponse> getAllPost();
 
-    @Query("SELECT p from Post p where p.category.categoryId = ?1")
-    List<PostResponse> getPostsByCategoryId(Long user_id);
+    @Query("""
+                SELECT p from Post p
+                inner join p.category c
+                where c.categoryId = :categoryId
+            """)
+    List<PostResponse> getPostsByCategoryId(Long categoryId);
 
     @Query("SELECT p FROM Post p WHERE p.postId=?1")
     public Post getPostsByPostId(Long postId);
 
-    @Query("SELECT p from Post p where p.users.username like ?1")
+    @Query("""
+                SELECT p from Post p
+                inner join p.users u
+                where u.username = :username
+            """)
     List<PostResponse> getPostByUsername(String username);
 
     @Query("SELECT p FROM Post p where concat(p.title, ' ', p.category.cname,' ',p.users.username) like %?1%")
-    List<PostResponse> search (String keyword);
+    List<PostResponse> search(String keyword);
 
     @Query("""
                select p from Post p
