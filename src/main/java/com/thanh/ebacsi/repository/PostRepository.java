a28f1d7159b0f789cacee2de4110125f36ceb39e
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:45:24 +0700

    PostRepo

---
+++ src/main/java/com/thanh/ebacsi/repository/PostRepository.java
@@ -0,0 +1,29 @@
+package com.thanh.ebacsi.repository;
+
+import com.thanh.ebacsi.dto.response.PostResponse;
+import com.thanh.ebacsi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByTitle(String title);
+
+    @Query("SELECT p from Post p ")
+    List<PostResponse> getAllPost();
+
+    @Query("SELECT p from Post p where p.category.categoryId = ?1")
+    List<PostResponse> getPostsByCategoryId(Long user_id);
+
+    @Query("SELECT p FROM Post p WHERE p.postId=?1")
+    public Post getPostsByPostId(Long postId);
+
+    @Query("SELECT p from Post p where p.users.username like ?1")
+    List<PostResponse> getPostByUsername(String username);
+
+    @Query("SELECT p FROM Post p where concat(p.title, ' ', p.category.cname,' ',p.users.username) like %?1%")
+    List<PostResponse> search (String keyword);
+}
