5291dbfdef0b2c9391f1b46da77281560676dc0e
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:53:07 +0700

    run

---
+++ src/test/java/com/thanh/ebacsi/EbacsiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanh.ebacsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbacsiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
