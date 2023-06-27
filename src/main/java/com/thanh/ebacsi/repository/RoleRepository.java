eb9da85b675e2eb237c83668cdc86af45623c786
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:47:38 +0700

    RoleRepo

---
+++ src/main/java/com/thanh/ebacsi/repository/RoleRepository.java
@@ -0,0 +1,27 @@
+package com.thanh.ebacsi.repository;
+
+import com.thanh.ebacsi.dto.response.RoleResponse;
+import com.thanh.ebacsi.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query("select r from Role r")
+    public List<RoleResponse> findAllRole();
+
+    @Query("select r from Role r where r.roleId = 2")
+    public Role getRoleDefault();
+
+    @Query("select r from Role r where r.roleId = ?1")
+    public Role getRoleByRoleId(Long roleId);
+
+    @Query("select r from Role r where r.roleName = :roleName")
+    public Role getRoleByRoleName(String roleName);
+
+
+}
