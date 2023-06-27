c80b46a295cf2e794346a2a5d06259ff36932be0
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:29:40 +0700

    role

---
+++ src/main/java/com/thanh/ebacsi/entity/Role.java
@@ -0,0 +1,40 @@
+package com.thanh.ebacsi.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.thanh.ebacsi.dto.request.RoleRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "role")
+    @JsonBackReference
+    private Set<User> user;
+
+    public Role(RoleRequest request) {
+        this.roleName = request.getRoleName();
+        this.roleId = request.getRoleId();
+    }
+//    public Role(List<String> roleNames) {
+//        // Iterate over the list of role names and create Role objects
+//        for (String roleName : roleNames) {
+//            this.roleName = roleName;
+//        }
+//    }
+}
