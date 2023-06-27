fc098328b132fa00893cd6033270b5e912427b13
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:52:40 +0700

    run

---
+++ src/main/java/com/thanh/ebacsi/EbacsiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thanh.ebacsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbacsiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbacsiProjectApplication.class, args);
+    }
+
+}
