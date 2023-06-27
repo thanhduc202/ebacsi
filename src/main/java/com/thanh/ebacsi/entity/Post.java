d82aae90d1453532f49d14e59bfef51f5bd496da
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:46:41 +0700

    Post

---
+++ src/main/java/com/thanh/ebacsi/entity/Post.java
@@ -0,0 +1,61 @@
+package com.thanh.ebacsi.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import com.thanh.ebacsi.dto.request.PostRequest;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.time.Instant;
+
+@Entity
+@Table(name = "post")
+
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+
+public class Post {
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "create_at")
+    private Instant createAt;
+    @Column(name = "update_at")
+    private Instant updateAt;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    @JsonBackReference
+    private User users;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    @JsonBackReference
+    private Category category;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "post_tag", joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = {@JoinColumn(name = "tag_id")}
+    )
+    @JsonBackReference
+    private Set<Tag> tag;
+
+    public Post(PostRequest request) {
+        this.title = request.getTitle();
+        this.content = request.getContent();
+    }
+
+
+}
