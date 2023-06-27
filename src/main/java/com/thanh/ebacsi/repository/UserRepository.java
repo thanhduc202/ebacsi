0246fae03bd5d1bbaf9a748401333aef2981aa23
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 08:50:04 +0700

    UserRepo

---
+++ src/main/java/com/thanh/ebacsi/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.thanh.ebacsi.repository;
+
+import com.thanh.ebacsi.entity.User;
+import com.thanh.ebacsi.dto.response.UserInfoResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByUsername(String username);
+
+    public User deleteUsersByUsername(String username);
+
+    @Query("select u from User u")
+    public List<UserInfoResponse> findAllUser();
+
+    @Query("select u from User u where u.userId=?1")
+    public User findByUserId(Long userId);
+}
+
