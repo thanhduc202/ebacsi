dcb201c7c071991c262d42463efdaeb138d77f83
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-06-27 10:29:59 +0700

    check user existed

--- src/main/java/com/thanh/ebacsi/service/user/UserServiceImpl.java
+++ src/main/java/com/thanh/ebacsi/service/user/UserServiceImpl.java
@@ -67,7 +67,6 @@ public class UserServiceImpl implements UserService {
         Optional<User> foundUser = userRepository.findById(id);
         if (!foundUser.isPresent()) {
             throw new NotFoundException("Not found user");
-
         }
         return foundUser.get();
     }
