62f7cb5572a853f9115e1fd0ef8d9e2f31b2b1b8
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-07-03 23:31:34 +0700

    format code

--- src/main/java/com/thanh/ebacsi/service/post/PostService.java
+++ src/main/java/com/thanh/ebacsi/service/post/PostService.java
@@ -1,4 +1,5 @@
 package com.thanh.ebacsi.service.post;
+
 import com.thanh.ebacsi.dto.request.PostRequest;
 import com.thanh.ebacsi.dto.response.PostResponse;
 import com.thanh.ebacsi.dto.response.ResponseObject;
@@ -8,26 +9,26 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 public interface PostService {
-    public Post save(Post post);
+    Post save(Post post);
 
     List<PostResponse> getAllPost();
 
-    public Post getPostsByPostId(Long postId);
+    Post getPostsByPostId(Long postId);
 
-    public Post insertPost(PostRequest postRequest, String token);
+    Post insertPost(PostRequest postRequest, String token);
 
-    public Post updatePost(PostRequest postRequest);
+    Post updatePost(PostRequest postRequest);
 
-    public List<PostResponse> findPostByCategoryId(Long categoryId);
+    List<PostResponse> findPostByCategoryId(Long categoryId);
 
-    public List<PostResponse> getPostByUsername(String username);
+    List<PostResponse> getPostByUsername(String username);
 
-    public List<PostResponse> search (String keyword);
+    List<PostResponse> search(String keyword);
 
     List<PostResponse> findPostByUserId(String token);
 
-    public Long getIdFromToken(String token);
+    Long getIdFromToken(String token);
 
-    public ResponseEntity<ResponseObject> deletePost(Long postId);
+    ResponseEntity<ResponseObject> deletePost(Long postId);
 
 }
