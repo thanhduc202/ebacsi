336dd3d658866871410435f5a4307a59dae4d374
thanh <ThanhNDHE160772@fpt.edu.vn>
2023-07-05 12:19:12 +0700

    format code

--- src/main/java/com/thanh/ebacsi/controller/CategoryController.java
+++ src/main/java/com/thanh/ebacsi/controller/CategoryController.java
@@ -11,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
 @RequestMapping("api/v1/category")
 public class CategoryController {
